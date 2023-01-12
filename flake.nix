6affa84723a45d1789789e8d0d25a782318698d1
Tom Houlé <tom@tomhoule.com>
2023-01-12 05:53:31 +0100

    flake: cosmetics

--- flake.nix
+++ flake.nix
@@ -5,33 +5,38 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
+      inherit (pkgs) duckdb;
     in
     {
-      packages."${system}" = let prefix = "${pkgs.duckdb}/bin/duckdb nutrition.db"; in {
-        opendb = pkgs.writeShellScriptBin "opendb" prefix;
+      packages."${system}" =
+        let
+          prefix = "${duckdb}/bin/duckdb nutrition.db";
+        in
+        {
+          opendb = pkgs.writeShellScriptBin "opendb" prefix;
 
-        recordWeight = pkgs.writeShellScriptBin "opendb" ''
-          ${prefix} "INSERT INTO weight (ts, weight_grams) VALUES ('$1', $2)"
-        '';
+          recordWeight = pkgs.writeShellScriptBin "opendb" ''
+            ${prefix} "INSERT INTO weight (ts, weight_grams) VALUES ('$1', $2)"
+          '';
 
-        duckdb = pkgs.duckdb;
+          show = pkgs.writeShellScriptBin "show" ''
+            ${prefix} << EOF
+              SELECT ts AS "when", weight_grams::float / 1000 AS chonk FROM weight ORDER BY created_at, ts ASC LIMIT 20;
 
-        show = pkgs.writeShellScriptBin "show" ''
-          ${prefix} << EOF
-            SELECT ts AS "when", weight_grams::float / 1000 AS chonk FROM weight ORDER BY created_at, ts ASC LIMIT 20;
-
-            WITH
-              one_week_ago AS (SELECT * FROM weight ORDER BY date_diff('hour', weight.ts, now()::timestamp - INTERVAL 7 DAY) DESC LIMIT 1)
-            SELECT 
-              printf('%.1f', avg(one_week_ago.weight_grams) / 1000) AS one_week_ago
-            FROM one_week_ago;
-          EOF
-        '';
-      };
+              WITH
+                one_week_ago AS (SELECT * FROM weight ORDER BY date_diff('hour', weight.ts, now()::timestamp - INTERVAL 7 DAY) DESC LIMIT 1)
+              SELECT 
+                printf('%.1f', avg(one_week_ago.weight_grams) / 1000) AS one_week_ago
+              FROM one_week_ago;
+            EOF
+          '';
+          
+          inherit duckdb;
+        };
 
 
       devShells."${system}".default = pkgs.mkShell {
-        packages = [ pkgs.duckdb ];
+        packages = [ duckdb ];
       };
     };
 }
