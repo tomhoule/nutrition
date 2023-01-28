5ca072572a0fc61a8d075ef6692e1bb856a0a3e8
Tom Houlé <tom@tomhoule.com>
2023-01-28 19:49:04 +0100

    dashboard: statistically cleaner regression

--- dashboard.sql
+++ dashboard.sql
@@ -6,30 +6,30 @@ WITH rows AS (
 )
 SELECT "when", weight FROM rows ORDER BY created_at ASC;
 
-WITH windowed_weight AS (
-  SELECT
-    *,
-    first_value(ts) OVER bucket AS first_ts,
-    last_value(ts) OVER bucket AS last_ts,
-    (epoch(first_ts) + epoch(last_ts)) / 2 AS mid_bucket,
-    regr_slope(weight_grams, epoch(ts) / (3600 * 24 * 7)) OVER rate_range AS rate,
-  FROM weight
-  WINDOW
-    bucket AS (PARTITION BY date_diff('weeks', now()::timestamp, ts)),
-    rate_range AS (
-      ORDER BY ts
-      RANGE BETWEEN INTERVAL 8 DAYS PRECEDING
-                AND INTERVAL 2 DAYS FOLLOWING
+WITH
+    bucketed_weight AS (
+        SELECT
+            *,
+            date_part('day', age(ts)) / 3 AS bucket, -- 2-day buckets
+            avg(weight_grams) OVER week AS naive_avg_weight,
+            avg(epoch(ts)) OVER week AS avg_ts,
+            regr_slope(weight_grams, epoch(ts)) OVER week AS raw_slope,
+            regr_slope(weight_grams, epoch(ts) / (3600 * 24 * 7)) OVER week AS rate,
+        FROM weight
+        WINDOW week AS (
+            ORDER BY ts
+            RANGE BETWEEN INTERVAL 3 DAYS PRECEDING
+                      AND INTERVAL 3 DAYS FOLLOWING
+        )
+        ORDER BY ts
     )
-  ORDER BY ts ASC
-)
 SELECT
-    to_timestamp(mid_bucket)::date AS date,
-    printf(
-      '%.2f',
-      regr_intercept(weight_grams, epoch(ts) - mid_bucket) / 1000
-    ) AS avg_weight,
-    printf('%+.2f', last(rate) / 1000) AS rate,
-FROM windowed_weight
-GROUP BY mid_bucket
-HAVING avg_weight IS NOT NULL;
+    first(ts)::date AS date,
+    printf('%.2f', first(naive_avg_weight) / 1000) AS naive_avg_weight,
+    -- See the regression intercept formula at
+    -- https://duckdb.org/docs/sql/aggregates#statistical-aggregates
+    printf('%.2f', (first(naive_avg_weight) - first(raw_slope) * (first(avg_ts) - first(epoch(ts)))) / 1000) AS regression_intercept,
+    printf('%+.2f', coalesce(first(rate) / 1000, 0)) AS weekly_rate,
+FROM bucketed_weight
+GROUP BY bucket
+ORDER BY bucket DESC
