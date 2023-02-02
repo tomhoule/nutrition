897c49bf9a7364c80d4662bb8e4c7d5eefeea550
Tom Houlé <tom@tomhoule.com>
2023-02-02 07:54:53 +0100

    dashboard: change slope formula
    Simpler. Also, it looks like we lost precision in the previous iteration.

--- dashboard.sql
+++ dashboard.sql
@@ -11,8 +11,7 @@ weight_windows AS (
     SELECT
         *,
         avg(weight_grams) OVER week AS naive_avg_weight,
-        regr_slope(weight_grams, epoch(ts)) OVER week AS raw_slope,
-        regr_slope(weight_grams, epoch(ts) / (3600 * 24 * 7)) OVER week AS rate,
+        regr_slope(weight_grams, epoch(ts)) OVER week AS slope,
         count(*) OVER week AS measurements_count,
         last(ts) OVER week AS last_ts,
         avg(epoch(ts)) OVER week AS avg_ts,
@@ -29,8 +28,7 @@ daily_weight_windows AS (
         first(ts) AS ts,
         first(naive_avg_weight) AS naive_avg_weight,
         first(measurements_count) AS measurements_count,
-        first(raw_slope) AS raw_slope,
-        first(rate) AS rate,
+        first(slope) AS slope,
         first(last_ts) AS last_ts,
         first(avg_ts) AS avg_ts,
     FROM weight_windows
@@ -42,8 +40,8 @@ SELECT
     printf('%.2f', naive_avg_weight / 1000) AS naive_avg_weight,
     -- See the regression intercept formula at
     -- https://duckdb.org/docs/sql/aggregates#statistical-aggregates
-    printf('%.2f', (naive_avg_weight - (raw_slope * (avg_ts - epoch(last_ts)))) / 1000) AS approx_weight,
-    printf('%+.2f', coalesce(rate / 1000, 0)) AS weekly_rate,
+    printf('%.2f', (naive_avg_weight - (slope * (avg_ts - epoch(last_ts)))) / 1000) AS approx_weight,
+    printf('%+.2f', coalesce(slope * 3600 * 24 * 7 / 1000, 0)) AS weekly_rate,
 FROM daily_weight_windows
 WHERE ts > now() - INTERVAL 2 MONTHS
   AND measurements_count > 4
