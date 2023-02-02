8e9bcfcc69cf4a9b188566ae5c4172d016e6eb0a
Tom Houlé <tom@tomhoule.com>
2023-02-02 07:34:21 +0100

    dashboard: change window to last 10 days
    This seems to make more intuitive sense.

--- dashboard.sql
+++ dashboard.sql
@@ -11,15 +11,16 @@ weight_windows AS (
     SELECT
         *,
         avg(weight_grams) OVER week AS naive_avg_weight,
-        avg(epoch(ts)) OVER week AS avg_ts,
         regr_slope(weight_grams, epoch(ts)) OVER week AS raw_slope,
         regr_slope(weight_grams, epoch(ts) / (3600 * 24 * 7)) OVER week AS rate,
         count(*) OVER week AS measurements_count,
+        last(ts) OVER week AS last_ts,
+        avg(epoch(ts)) OVER week AS avg_ts,
     FROM weight
     WINDOW week AS (
         ORDER BY ts
-        RANGE BETWEEN INTERVAL 3 DAYS PRECEDING
-                  AND INTERVAL 3 DAYS FOLLOWING
+        RANGE BETWEEN INTERVAL 9 DAYS PRECEDING
+                  AND CURRENT ROW
     )
     ORDER BY ts
 ),
@@ -30,6 +31,7 @@ daily_weight_windows AS (
         first(measurements_count) AS measurements_count,
         first(raw_slope) AS raw_slope,
         first(rate) AS rate,
+        first(last_ts) AS last_ts,
         first(avg_ts) AS avg_ts,
     FROM weight_windows
     GROUP BY ts::DATE
@@ -40,10 +42,9 @@ SELECT
     printf('%.2f', naive_avg_weight / 1000) AS naive_avg_weight,
     -- See the regression intercept formula at
     -- https://duckdb.org/docs/sql/aggregates#statistical-aggregates
-    printf('%.2f', (naive_avg_weight - raw_slope * (avg_ts - epoch(ts))) / 1000) AS regression_intercept,
+    printf('%.2f', (naive_avg_weight - (raw_slope * (avg_ts - epoch(last_ts)))) / 1000) AS approx_weight,
     printf('%+.2f', coalesce(rate / 1000, 0)) AS weekly_rate,
 FROM daily_weight_windows
 WHERE ts > now() - INTERVAL 2 MONTHS
-  AND ts < now() - INTERVAL 3 DAYS
   AND measurements_count > 4
 ORDER BY ts
