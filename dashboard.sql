7a1da62644b51ea43e806851ff24c840fe780ec9
Tom Houlé <tom@tomhoule.com>
2023-03-06 07:00:45 +0100

    dashboard: simplify slope calculation
    More averages, less guessing. Over less data, so it is bound to vary a
    bit more.

--- dashboard.sql
+++ dashboard.sql
@@ -10,25 +10,24 @@ with_recent_weight AS (
         avg(daily_avg_weight)
             OVER (
                 ORDER BY date
-                RANGE BETWEEN INTERVAL 13 DAYS PRECEDING
+                RANGE BETWEEN INTERVAL 6 DAYS PRECEDING
                           AND CURRENT ROW
             )
             AS recent_avg_weight,
+        avg(daily_avg_weight)
+            OVER (
+                ORDER BY date
+                RANGE BETWEEN INTERVAL 11 DAYS PRECEDING
+                          AND INTERVAL 6 DAYS PRECEDING
+            )
+            AS last_week_weight,
     FROM daily_weight
-),
-with_slope AS (
-    SELECT
-        *,
-        recent_avg_weight - lag(recent_avg_weight)
-            OVER (ORDER BY date)
-            AS estimated_weekly_slope,
-    FROM with_recent_weight
 )
 SELECT
     date,
     printf('%.2f', daily_avg_weight / 1000) AS daily_avg_weight,
     printf('%.2f', recent_avg_weight / 1000) AS recent_avg_weight,
-    printf('%+.2f', (estimated_weekly_slope * 7) / 1000) AS estimated_weekly_slope,
-FROM with_slope
+    printf('%+.2f', (recent_avg_weight - last_week_weight) / 1000) AS estimated_weekly_slope,
+FROM with_recent_weight
 WHERE date > today() - INTERVAL 35 DAYS
 ORDER BY date ASC
