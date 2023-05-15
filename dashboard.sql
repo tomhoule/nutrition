7c67a24d45e9c633744298b88d35c5a3e1818abe
Tom Houlé <tom@tomhoule.com>
2023-05-15 06:55:05 +0200

    dashboard: use 10 day instead of 7 day windows
    Daily fluctuations are too high for a 7 day window to be reliable. This
    should smooth out the slope estimations a little bit.

--- dashboard.sql
+++ dashboard.sql
@@ -10,24 +10,24 @@ with_recent_weight AS (
         avg(daily_avg_weight)
             OVER (
                 ORDER BY date
-                RANGE BETWEEN INTERVAL 6 DAYS PRECEDING
+                RANGE BETWEEN INTERVAL 10 DAYS PRECEDING
                           AND CURRENT ROW
             )
             AS recent_avg_weight,
         avg(daily_avg_weight)
             OVER (
                 ORDER BY date
-                RANGE BETWEEN INTERVAL 11 DAYS PRECEDING
-                          AND INTERVAL 6 DAYS PRECEDING
+                RANGE BETWEEN INTERVAL 20 DAYS PRECEDING
+                          AND INTERVAL 10 DAYS PRECEDING
             )
-            AS last_week_weight,
+            AS previous_avg_weight,
     FROM daily_weight
 )
 SELECT
     date,
     printf('%.2f', daily_avg_weight / 1000) AS daily_avg_weight,
     printf('%.2f', recent_avg_weight / 1000) AS recent_avg_weight,
-    printf('%+.2f', (recent_avg_weight - last_week_weight) / 1000) AS estimated_weekly_slope,
+    printf('%+.2f', ((recent_avg_weight - previous_avg_weight) * 7 / 10) / 1000) AS estimated_weekly_slope,
 FROM with_recent_weight
 WHERE date > today() - INTERVAL 35 DAYS
 ORDER BY date ASC
